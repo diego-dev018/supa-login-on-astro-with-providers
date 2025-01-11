f39b71d6a9135b35b8d498561791f83e840d9bb9
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-01-11 13:43:06 -0600

    Refactor Vercel adapter import in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercelAdapter from '@astrojs/vercel';
+import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercelAdapter(),
+  adapter: vercel(),
   integrations: [tailwind()]
 });
